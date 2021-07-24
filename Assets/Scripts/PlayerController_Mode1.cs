e278181c656cc934bc449f7a3fdcc94983cd7659
David Della Rossa <david.dellarossa@gmail.com>
2021-07-24 16:53:03 +0100

    Player controller, input system and camera

--- Assets/Scripts/PlayerController_Mode1.cs
+++ Assets/Scripts/PlayerController_Mode1.cs
@@ -11,8 +11,11 @@ public class PlayerController_Mode1 : MonoBehaviour
     public CharacterController characterController;
     public Camera mainCamera;
     private Vector3 _moveDirection;
-    public float turnSmoothTime = 0.1f;
+    public float turnSmoothTime = 1.1f;
     private float turnSmoothVelocity;
+    
+    //Camera
+    [SerializeField] private Vector3 _cameraRelPosition;
 
     public void FixedUpdate()
     {
@@ -35,22 +38,42 @@ public class PlayerController_Mode1 : MonoBehaviour
 
     private void Method1()
     {
-        var cameraForward = mainCamera.transform.forward;
+        var cameraTransform = mainCamera.transform;
+        var playerTransform = transform;
+
+        //Get the forward direction respect to camera ( this points slightly down, therefore we set y = 0)
+        var cameraForward = cameraTransform.forward;
         cameraForward.y = 0;
         
+        //Relative rotation of camera respect to world
         var cameraRelativeRotation = Quaternion.FromToRotation(Vector3.forward, cameraForward);
+        
+        //Target look direction based on user input
         var lookToward = cameraRelativeRotation * _moveDirection;
         
+        //If there is input
         if (_moveDirection.sqrMagnitude > 0)
         {
-            var lookRay = new Ray(transform.position, lookToward);
-            transform.LookAt(lookRay.GetPoint(1));
+            //calculate the rotation angle to go from current direction to target direction
+            var targetAngle = Vector3.SignedAngle(Vector3.forward, lookToward, Vector3.up);
+            var targetRotation = Quaternion.Euler(0, targetAngle, 0);
+            //smooth rotation so as not to have sharp movements
+            var smoothedRotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSmoothTime);
+            //apply rotation
+            transform.rotation = smoothedRotation;
         }
         
-        var moveVelocity = transform.forward * (moveSpeed * _moveDirection.sqrMagnitude);
-
+        //Normalize speed
+        var moveVelocity = playerTransform.forward * (moveSpeed * _moveDirection.sqrMagnitude);
         characterController.SimpleMove(moveVelocity);
         
+        //Camera orientation
+        var vectorCameraToPlayer = playerTransform.position - _cameraRelPosition;
+        cameraTransform.position = vectorCameraToPlayer;
+        
+        //Camera always looks at player
+        cameraTransform.LookAt(playerTransform);
+
     }
     
 }
--- Assets/Scripts/PlayerController_Mode2.cs
+++ Assets/Scripts/PlayerController_Mode2.cs
@@ -61,8 +61,7 @@ public class PlayerController_Mode2 : MonoBehaviour
             return;
         
         float targetAngle = Mathf.Atan2(_moveDirection.x, _moveDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
-        float angle =
-            Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
         var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
