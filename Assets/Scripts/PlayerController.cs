8709e1839ccc647098fec734dd2e1f9ada82f684
David Della Rossa <david.dellarossa@gmail.com>
2021-07-24 17:53:08 +0100

    Scene cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,17 @@ using UnityEngine.Serialization;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public CharacterController characterController;
-    public Camera mainCamera;
-    private Vector3 _moveDirection;
-    public float turnSmoothTime = 0.1f;
-    private float turnSmoothVelocity;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    //Camera
+    [SerializeField] private Vector3 cameraRelPosition;
     
+    private Vector3 _moveDirection;
 
     public void FixedUpdate()
     {
-        //Method1_From_PS();
-        Method2_From_Brackeys();
-
+        MovePlayerAndCamera();
     }
 
     public void Fire(InputAction.CallbackContext context)
@@ -35,37 +34,45 @@ public class PlayerController : MonoBehaviour
         _moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
     }
 
-    private void Method1_From_PS()
+    private void MovePlayerAndCamera()
     {
-        var cameraForward = GetComponent<Camera>().transform.forward;
+        var cameraTransform = mainCamera.transform;
+        var playerTransform = transform;
+
+        //Get the forward direction respect to camera ( this points slightly down, therefore we set y = 0)
+        //var cameraForward = cameraTransform.forward;
+        var cameraForward = cameraTransform.forward;
         cameraForward.y = 0;
         
+        //Relative rotation of camera respect to world
         var cameraRelativeRotation = Quaternion.FromToRotation(Vector3.forward, cameraForward);
+        
+        //Target look direction based on user input
         var lookToward = cameraRelativeRotation * _moveDirection;
         
+        //If there is input
         if (_moveDirection.sqrMagnitude > 0)
         {
-            var lookRay = new Ray(transform.position, lookToward);
-            transform.LookAt(lookRay.GetPoint(1));
+            //calculate the rotation angle to go from current direction to target direction
+            var targetAngle = Vector3.SignedAngle(Vector3.forward, lookToward, Vector3.up);
+            var targetRotation = Quaternion.Euler(0, targetAngle, 0);
+            //smooth rotation so as not to have sharp movements
+            var smoothedRotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSmoothTime);
+            //Debug.Log($"Target: {targetRotation}, smoothed: {smoothedRotation}");
+            //apply rotation
+            transform.rotation = smoothedRotation;
         }
         
-        var moveVelocity = transform.forward * moveSpeed * _moveDirection.sqrMagnitude;
-        //moveVelocity = new Vector3(inputVector.x, 0f, inputVector.y);
-        
+        //Normalize speed
+        var moveVelocity = playerTransform.forward * (moveSpeed * _moveDirection.sqrMagnitude);
         characterController.SimpleMove(moveVelocity);
-    }
-
-    private void Method2_From_Brackeys()
-    {
-        if (_moveDirection.magnitude == 0)
-            return;
         
-        float targetAngle = Mathf.Atan2(_moveDirection.x, _moveDirection.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
-        float angle =
-            Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //Camera orientation
+        var vectorCameraToPlayer = playerTransform.position - cameraRelPosition;
+        cameraTransform.position = vectorCameraToPlayer;
+        
+        //Camera always looks at player
+        cameraTransform.LookAt(playerTransform);
 
-        var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        characterController.SimpleMove(moveDir.normalized * moveSpeed);
     }
 }
