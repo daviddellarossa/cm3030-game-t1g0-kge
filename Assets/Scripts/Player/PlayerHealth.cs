3d1b21b68e4d564066f523a186a1a3325e5465aa
David Della Rossa <david.dellarossa@gmail.com>
2021-08-14 20:45:50 +0100

    Player's health

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth;
+
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    public UnityEvent PlayerIsDead;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            PlayerIsDead?.Invoke();
+        }
+        
+    }
+    
+    public void TakeDamageAction(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Damage taken");
+            TakeDamage(20);
+        }
+    }
+}
