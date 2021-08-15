76d797f735ee8ffc09f60473b3d141e7fdace3e1
Chen Ni <nichen0301@sina.com>
2021-08-15 20:25:26 +0800

    fix bug

---
+++ Assets/Scripts/Foe/EnemyPurchasingPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPurchasingPlayer : MonoBehaviour
+{
+    private NavMeshAgent Mob;
+
+    public GameObject Player;
+
+    public float MobDistanceRun = 4.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mob = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        
+        // Run towards player
+
+        if (distance < MobDistanceRun)
+        {
+            Mob.SetDestination(Player.transform.position);
+        }
+    }
+}
