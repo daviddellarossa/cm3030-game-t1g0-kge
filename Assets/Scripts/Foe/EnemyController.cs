684cec3f6c6f95ff212e4b729ffe2c29fc410448
Chen Ni <nichen0301@sina.com>
2021-08-22 18:22:50 +0800

    fix bug

--- Assets/Scripts/Foe/EnemyController.cs
+++ Assets/Scripts/Foe/EnemyController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NavMeshAgentController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public GameObject[] patrolPoints;
     public GameObject Player;
