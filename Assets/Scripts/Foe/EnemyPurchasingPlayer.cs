3cd098ad6cf32cbd5e393244deb2a3ad016ab587
Chen Ni <nichen0301@sina.com>
2021-08-22 17:24:18 +0800

    update enemy

--- Assets/Scripts/Foe/EnemyPurchasingPlayer.cs
+++ Assets/Scripts/Foe/EnemyPurchasingPlayer.cs
@@ -5,29 +5,18 @@ using UnityEngine.AI;
 
 public class EnemyPurchasingPlayer : MonoBehaviour
 {
-    private NavMeshAgent Mob;
 
-    public GameObject Player;
 
-    public float MobDistanceRun = 4.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        Mob = GetComponent<NavMeshAgent>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, Player.transform.position);
         
-        // Run towards player
-
-        if (distance < MobDistanceRun)
-        {
-            Mob.SetDestination(Player.transform.position);
-        }
     }
 }
--- Assets/Scripts/Foe/NavMeshAgentController.cs
+++ Assets/Scripts/Foe/NavMeshAgentController.cs
@@ -6,30 +6,63 @@ using UnityEngine.AI;
 public class NavMeshAgentController : MonoBehaviour
 {
     public GameObject[] patrolPoints;
+    public GameObject Player;
+    private NavMeshAgent Mob;
+    public float EnemySightRange = 4.0f;
+
     private int destinationIndex = 0;
     private GameObject destination;
     
     // Start is called before the first frame update
     void Start()
     {
-        SetDestination();
+        Mob = GetComponent<NavMeshAgent>();
+
+        chooseDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        
+        if (distance < EnemySightRange)
+        {
+            chasePlayer();
+        }
+        else
+        {
+            patrol();
+        }
+
+    }
+
+    void patrol()
+    {
+        SetDestination();
+        
         if (ArrivedAtDestination())
         {
-            destinationIndex = (destinationIndex + 1) % patrolPoints.Length;
-            SetDestination();
+            chooseDestination();
         }
+    }
+
+    void chasePlayer()
+    {
+        transform.LookAt(Player.transform);
+        Mob.SetDestination(Player.transform.position);
+    }
+
+    void chooseDestination()
+    {
+        destinationIndex = (destinationIndex + 1) % patrolPoints.Length;
+        destination = patrolPoints[destinationIndex];
     }
 
     void SetDestination()
     {
-        Debug.Log("asdfpoiu");
-        destination = patrolPoints[destinationIndex];
-        GetComponent<NavMeshAgent>().SetDestination(new Vector3(
+        Mob.SetDestination(new Vector3(
             destination.transform.position.x,
             transform.position.y,
             destination.transform.position.z));
