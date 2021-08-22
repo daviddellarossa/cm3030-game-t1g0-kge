9335dc367d9d5a66cfcdb8556115ed6da30cf0df
gsitkina7 <64528089+gsitkina7@users.noreply.github.com>
2021-08-22 10:04:35 +0400

    Create Inventory Objects

---
+++ Assets/Scriptable Objects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    HealthPack,
+    Battery,
+    Default
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type;
+    [TextArea(15, 20)]
+    public string description;
+}
