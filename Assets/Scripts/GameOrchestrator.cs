c2e6fd8590c6c95e8ea850bf57b597624e6b2045
David Della Rossa <david.dellarossa@gmail.com>
2021-08-25 08:03:41 +0100

    Message displayer

--- Assets/Scripts/GameOrchestrator.cs
+++ Assets/Scripts/GameOrchestrator.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -41,5 +43,35 @@ public class GameOrchestrator : MonoBehaviour
     {
         Debug.Log($"GameObject {gameObject.name} dies");
         Destroy(gameObject);
+    }
+
+    public void HintTriggereEnter_EventHandler(string message)
+    {
+        Debug.Log(message);
+        var hintDisplay = GameObject.FindGameObjectWithTag("HintDisplay");
+        var textMeshPro = hintDisplay.GetComponent<TextMeshProUGUI>();
+        textMeshPro.text = message;
+        Invoke(nameof(ClearMessage), 3);
+    }
+    
+    public void HintTriggereEnter2_EventHandler(TextMeshPro message)
+    {
+        Debug.Log(message);
+        //var hintDisplay = GameObject.FindGameObjectWithTag("HintDisplay");
+        //var canvasRenderer = hintDisplay.GetComponent<CanvasRenderer>();
+        //message.renderer = canvasRenderer;
+        //Invoke(nameof(ClearMessage), 3);
+    }
+    
+    public void HintTriggereExit_EventHandler()
+    {
+        //Debug.Log("Clear message");
+    }
+
+    public void ClearMessage()
+    {
+        var hintDisplay = GameObject.FindGameObjectWithTag("HintDisplay");
+        var textMeshPro = hintDisplay.GetComponent<TextMeshProUGUI>();
+        textMeshPro.text = String.Empty;
     }
 }
---
+++ Assets/Scripts/HintTrigger/HintTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class HintTriggeredEnterEvent : UnityEvent<string>{}
+
+[Serializable]
+public class HintTriggeredEnterEvent2 : UnityEvent<TextMeshPro>{}
+
+public class HintTrigger : MonoBehaviour
+{
+    public string message;
+    public bool fireOnlyOnce = false;
+    public bool fired = false;
+    
+    public HintTriggeredEnterEvent HintTriggeredEnter;
+    public HintTriggeredEnterEvent2 HintTriggeredEnter2;
+    
+    public UnityEvent HintTriggeredExit;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!(fireOnlyOnce & fired))
+        {
+            HintTriggeredEnter?.Invoke(message);
+            var tmp = GetComponent<TextMeshPro>();
+            HintTriggeredEnter2?.Invoke(tmp);
+            fired = true;
+        }
+    }
+
+    private void BuildMessage()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!(fireOnlyOnce & fired))
+        {
+            HintTriggeredExit?.Invoke();
+        }
+    }
+
+}
