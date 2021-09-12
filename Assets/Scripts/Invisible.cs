f7f9fde9df5d0e0c48a30aac871612e9983d5bf8
Liam Pender <50781082+lpenderUoL@users.noreply.github.com>
2021-09-12 21:20:46 +0100

    Invisible build 1

---
+++ Assets/Scripts/Invisible.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour
+{
+
+    
+    public Material[] material;
+    public bool isInvis;
+    Renderer[] rend;
+
+    void Start()
+    {
+        rend = GetComponentsInChildren<Renderer>();
+        foreach(var r in rend)
+        {
+            r.enabled = true;
+            r.sharedMaterial = r.materials[0];
+        }
+        
+        
+        isInvis = false;
+        
+    }
+
+    public void makeInvisible(/*InputAction.CallbackContext context*/)
+    {
+        Debug.Log("invs");
+
+        if (!isInvis) {
+            foreach (var r in rend)
+            {
+                r.sharedMaterial = material[1];
+            }
+      
+            Debug.Log("inside the if");
+            isInvis = !isInvis;
+            Invoke(nameof(makeVisible), 5.0f);
+        }
+              
+        
+    }
+
+    public void makeVisible()
+    {
+        Debug.Log("inside make vis");
+        foreach (var r in rend)
+        {
+            r.sharedMaterial = material[0];
+        }
+        isInvis = false;
+    }
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    
+}
